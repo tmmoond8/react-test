8f9cdd55a7b970e7fe471c22fe1fe3e18463fbe5
TaeminMoon <tmmoond8@gmail.com>
2020-09-20 21:46:44 +0900

    feat: setup storybook

---
+++ src/components/Footer/Footer.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {} from "@storybook/addon-actions";
+import Footer from ".";
+
+export default {
+  title: "Footer",
+  component: Footer,
+};
+
+export const Default = () => {
+  return <Footer />;
+};
